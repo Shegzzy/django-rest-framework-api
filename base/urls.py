06218e7c3f31c70aa74ed3f1100e7e1819bf2839
Shegzzy <miksegun1996@gmail.com>
2023-08-05 22:55:57 +0100

    added SimpleJWT and tested token in postman for auth

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path("", views.endpoints),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("advocates/", views.advocate_list, name="advocates"),
     # path("advocates/<str:username>/", views.AdvocateDetail.as_view()),
     path("advocates/<str:username>/", views.advocate_detail),
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
@@ -17,6 +18,7 @@ def endpoints(request):
 
 
 @api_view(["GET", "POST"])
+@permission_classes([IsAuthenticated])
 def advocate_list(request):
     # Handle GET request
     if request.method == "GET":
