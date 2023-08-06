d48210b0aa4e7542139f5173071bd8589d671d55
Shegzzy <miksegun1996@gmail.com>
2023-08-06 15:25:06 +0100

    installed django-cors header and setting allowed server to connect to the api

--- base/views.py
+++ base/views.py
@@ -18,7 +18,7 @@ def endpoints(request):
 
 
 @api_view(["GET", "POST"])
-@permission_classes([IsAuthenticated])
+# @permission_classes([IsAuthenticated])
 def advocate_list(request):
     # Handle GET request
     if request.method == "GET":
